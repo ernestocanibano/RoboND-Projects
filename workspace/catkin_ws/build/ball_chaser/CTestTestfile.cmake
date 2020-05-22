f6c1e94a5c60b33e749c076d3568fe7bc82e4236
ernestocanibano <ernestoecr@gmail.com>
2020-05-22 16:51:21 +0200

    chore: Test project

---
+++ workspace/catkin_ws/build/ball_chaser/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/src/ball_chaser
+# Build directory: /home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/build/ball_chaser
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ workspace/catkin_ws/devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(ball_chaser_MESSAGE_FILES "")
+set(ball_chaser_SERVICE_FILES "/home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/src/ball_chaser/srv/DriveToTarget.srv")
