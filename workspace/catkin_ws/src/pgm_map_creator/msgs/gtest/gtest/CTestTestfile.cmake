55ace20ef21cc441027f01dfc97ccb1fc17c57ea
ernestocanibano <ernestoecr@gmail.com>
2020-05-22 18:04:11 +0200

    test2

---
+++ workspace/catkin_ws/src/pgm_map_creator/msgs/gtest/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/src/pgm_map_creator/msgs/gtest/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
