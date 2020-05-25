ec7a1b2b39bc17b9bead636430532b42d233c0d0
ernestocanibano <ernestoecr@gmail.com>
2020-05-25 23:25:20 +0200

    feat: Project submsion

--- workspace/catkin_ws/build/catkin_generated/setup_cached.sh
+++ workspace/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -8,9 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/usr/local/cuda/extras/CUPTI/lib64"
-export PATH="/opt/ros/kinetic/bin:/home/ernesto/embedded_linux/bin:/home/ernesto/bin:/home/ernesto/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/cuda/bin:/snap/bin:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin"
+export LD_LIBRARY_PATH="/home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
 export PKG_CONFIG_PATH="/home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/build"
 export PYTHONPATH="/home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
-export ROS_PACKAGE_PATH="/home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/src:$ROS_PACKAGE_PATH"
+export ROS_PACKAGE_PATH="/home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/src:/home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/src:/home/ernesto/udacity/robotics/_Project3/workspace/catkin_ws/src:/opt/ros/kinetic/share"
--- workspace/catkin_ws/build/pgm_map_creator/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/src/pgm_map_creator
-# Build directory: /home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/build/pgm_map_creator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(msgs)
