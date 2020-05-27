a6ec55e10e7b0e67dc04825d5d12fc8169e9384d
ernestocanibano <ernestoecr@gmail.com>
2020-05-27 07:54:31 +0200

    feat: Added teleop.launch

--- workspace/catkin_ws/build/CTestTestfile.cmake
+++ workspace/catkin_ws/build/CTestTestfile.cmake
@@ -7,4 +7,3 @@
 subdirs(gtest)
 subdirs(my_robot)
 subdirs(teleop_twist_keyboard)
-subdirs(pgm_map_creator)
--- workspace/catkin_ws/build/cmake_install.cmake
+++ workspace/catkin_ws/build/cmake_install.cmake
@@ -128,7 +128,6 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("/home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/build/gtest/cmake_install.cmake")
   include("/home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/build/my_robot/cmake_install.cmake")
   include("/home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/build/teleop_twist_keyboard/cmake_install.cmake")
-  include("/home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/build/pgm_map_creator/cmake_install.cmake")
 
 endif()
 
