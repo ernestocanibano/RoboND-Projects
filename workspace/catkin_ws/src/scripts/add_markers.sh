985ab9d98ed1edac507bece9fdc280152ab71df6
ernestocanibano <ernestoecr@gmail.com>
2020-05-28 19:36:57 +0200

    feat: Second submission

--- workspace/catkin_ws/src/scripts/add_markers.sh
+++ workspace/catkin_ws/src/scripts/add_markers.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/src/map/myworld.world" & 
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find turtlebot_gazebo)/../../map/myworld.world" & 
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/src/map/map.yaml " &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find turtlebot_gazebo)/../../map/map.yaml " &
 sleep 5
 xterm -e " rosrun add_markers add_markers _mode:=TEST" &
 sleep 5
--- workspace/catkin_ws/src/scripts/home_service.sh
+++ workspace/catkin_ws/src/scripts/home_service.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/src/map/myworld.world" & 
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find turtlebot_gazebo)/../../map/myworld.world" & 
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/ernesto/udacity/robotics/Project3/workspace/catkin_ws/src/map/map.yaml " &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find turtlebot_gazebo)/../../map/map.yaml " &
 sleep 5
 xterm -e " rosrun add_markers add_markers " &
 sleep 5
-xterm -e " roslaunch ../rvizConfig/view_navigation_custom.launch" &
+xterm -e " roslaunch $(rospack find turtlebot_gazebo)/../../rvizConfig/view_navigation_custom.launch" &
 sleep 5
 xterm -e " rosrun pick_objects pick_objects"  
